eff736b32b0da2f60ea4391a7e7132f5c21ead97
Chloe Lee (DCS-RD-TW) <chloe_lee@trend.com.tw>
2015-12-22 14:17:55 +0800

    app usage

---
+++ rpm-app/recipes/deploy.rb
@@ -0,0 +1,15 @@
+include_recipe 'deploy'
+
+node[:deploy].each do |app_name, deploy_config|
+  # determine root folder of new app deployment
+  app_root = "#{deploy_config[:deploy_to]}/current"
+
+ # install & start 
+  bash 'deploy-rpm' do
+        owner deploy_config[:user]
+        code <<-EOH
+                rpm -ivh "#{app_root}/#{deploy[:application]}*"
+        EOH
+        notifies :start, "service[#{node['rpm']['serviceName']}]", :delayed
+  end
+end
