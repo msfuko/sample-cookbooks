bf79df02949a49bc2abe77808fd170b51fb70a6e
Chloe Lee (DCS-RD-TW) <chloe_lee@trend.com.tw>
2015-12-22 14:56:32 +0800

    fix empty service

--- rpm-app/recipes/deploy.rb
+++ rpm-app/recipes/deploy.rb
@@ -4,19 +4,19 @@ node[:deploy].each do |app_name, deploy_config|
   # determine root folder of new app deployment
   app_root = "#{deploy_config[:deploy_to]}/current"
   
-  Chef::Log.info("deploy #{deploy_config[:app_name]}")
+  Chef::Log.info("deploy #{app_name}")
 
   # install
   bash 'deploy-rpm' do
         user deploy_config[:user]
         code <<-EOH
-                rpm -ivh "#{app_root}/#{deploy_config[:app_name]}*"
+                rpm -ivh "#{app_root}/#{app_name}*"
         EOH
-        notifies :start, "service[#{deploy_config[:app_name]}]", :delayed
+        notifies :start, "service[#{app_name}]", :delayed
   end
   
   # start
-  service '#{deploy_config[:app_name]}' do
+  service '#{app_name}' do
         action :nothing
   end
 end
--- rpm-app/recipes/undeploy.rb
+++ rpm-app/recipes/undeploy.rb
@@ -2,7 +2,7 @@ include_recipe 'deploy'
 
 # stop the service
 node[:deploy].each do |app_name, deploy_config|
-  service '#{deploy_config[:app_name]}' do
+  service '#{app_name}' do
           action :stop
   end
 end  
