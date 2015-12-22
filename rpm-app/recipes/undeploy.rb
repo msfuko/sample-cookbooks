486259d7f2531fb904ded65bdfd329ce0852a38a
Chloe Lee (DCS-RD-TW) <chloe_lee@trend.com.tw>
2015-12-22 19:42:34 +0800

    add undeploy

--- rpm-app/recipes/undeploy.rb
+++ rpm-app/recipes/undeploy.rb
@@ -3,8 +3,17 @@ include_recipe 'deploy'
 # stop the service
 node[:deploy].each do |app_name, deploy_config|
   service 'rpm' do
-          service_name "#{app_name}"
-          action :stop
+        service_name "#{app_name}"
+        action :stop
+        notifies :run, "bash[undeploy-rpm]", :delayed
+  end
+
+  # uninstall
+  bash 'undeploy-rpm' do
+        user 'root'
+        code <<-EOH
+                rpm -e "#{app_name}"
+        EOH
   end
 end  
 
