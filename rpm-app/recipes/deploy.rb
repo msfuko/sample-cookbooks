cf8c809c3dc8b9a8cdedfa6d2f1fd766ee5e3456
Chloe Lee (DCS-RD-TW) <chloe_lee@trend.com.tw>
2015-12-22 14:50:43 +0800

    add log

--- rpm-app/recipes/deploy.rb
+++ rpm-app/recipes/deploy.rb
@@ -3,6 +3,8 @@ include_recipe 'deploy'
 node[:deploy].each do |app_name, deploy_config|
   # determine root folder of new app deployment
   app_root = "#{deploy_config[:deploy_to]}/current"
+  
+  Chef::Log.info("deploy #{deploy_config[:app_name]}")
 
   # install
   bash 'deploy-rpm' do
@@ -10,7 +12,7 @@ node[:deploy].each do |app_name, deploy_config|
         code <<-EOH
                 rpm -ivh "#{app_root}/#{deploy_config[:app_name]}*"
         EOH
-        notifies :start, "service[#{node['rpm']['serviceName']}]", :delayed
+        notifies :start, "service[#{deploy_config[:app_name]}]", :delayed
   end
   
   # start
