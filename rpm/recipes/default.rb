59f89a6d9b75d06640539a09cfc405099d7b8e67
Chloe Lee (DCS-RD-TW) <chloe_lee@trend.com.tw>
2015-12-22 13:43:42 +0800

    + rpm -ivh

--- rpm/recipes/default.rb
+++ rpm/recipes/default.rb
@@ -1,13 +1,26 @@
+# download rpm from s3
 bash 'download_rpm' do
         user 'root'
         cwd '/tmp'
         code <<-EOH
                 aws s3 cp #{node["rpm"]["sourceUrl"]} .
         EOH
-        notifies :start, service["#{node["rpm"]["serviceName"]}"], :delayed
+        notifies :run, 'bash[install_rpm]', :delayed
 end
 
-service "#{node["rpm"]["serviceName"]}" do
+# install rpm
+bash 'install_rpm' do
+        user 'root'
+        cwd '/tmp'
+        code <<-EOH
+                rpm -ivh "#{node["rpm"]["serviceName"]}*"
+        EOH
+        notifies :start, "service[#{node['rpm']['serviceName']}]", :delayed
+    
+end
+
+# start the service
+service '#{node["rpm"]["serviceName"]}' do
         action :nothing
 end
 
