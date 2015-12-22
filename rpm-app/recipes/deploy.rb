36e024b0b9bea38e687d9141230204ab416c4953
Chloe Lee (DCS-RD-TW) <chloe_lee@trend.com.tw>
2015-12-22 14:37:02 +0800

    update recipe

--- rpm-app/recipes/deploy.rb
+++ rpm-app/recipes/deploy.rb
@@ -8,7 +8,7 @@ node[:deploy].each do |app_name, deploy_config|
   bash 'deploy-rpm' do
         user deploy_config[:user]
         code <<-EOH
-                rpm -ivh "#{app_root}/#{deploy[:application]}*"
+                rpm -ivh "#{app_root}/#{deploy_config[:app_name]}*"
         EOH
         notifies :start, "service[#{node['rpm']['serviceName']}]", :delayed
   end
--- rpm-app/recipes/undeploy.rb
+++ rpm-app/recipes/undeploy.rb
@@ -1,7 +1,9 @@
 include_recipe 'deploy'
 
 # stop the service
-service '#{deploy[:application]}' do
-        action :stop
-end
+node[:deploy].each do |app_name, deploy_config|
+  service '#{deploy_config[:app_name]}' do
+          action :stop
+  end
+end  
 
