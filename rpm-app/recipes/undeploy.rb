d804806238d7a0a1b746d69ed874960dd110a896
Chloe Lee (DCS-RD-TW) <chloe_lee@trend.com.tw>
2015-12-22 21:19:25 +0800

    remove rpm -e

--- rpm-app/recipes/undeploy.rb
+++ rpm-app/recipes/undeploy.rb
@@ -5,15 +5,15 @@ node[:deploy].each do |app_name, deploy_config|
   service 'rpm' do
         service_name "#{app_name}"
         action :stop
-        notifies :run, "bash[undeploy-rpm]", :delayed
+  #      notifies :run, "bash[undeploy-rpm]", :delayed
   end
 
   # uninstall
-  bash 'undeploy-rpm' do
-        user 'root'
-        code <<-EOH
-                rpm -e "#{app_name}"
-        EOH
-  end
+  #bash 'undeploy-rpm' do
+  #      user 'root'
+  #      code <<-EOH
+  #              rpm -e "#{app_name}"
+  #      EOH
+  #end
 end  
 
