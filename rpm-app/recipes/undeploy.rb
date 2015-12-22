a446e31c5189cb48e6416397f6c7dacf047dd294
Chloe Lee (DCS-RD-TW) <chloe_lee@trend.com.tw>
2015-12-22 19:38:21 +0800

    change var

--- rpm-app/recipes/undeploy.rb
+++ rpm-app/recipes/undeploy.rb
@@ -3,7 +3,7 @@ include_recipe 'deploy'
 # stop the service
 node[:deploy].each do |app_name, deploy_config|
   service 'rpm' do
-          service_name '#{app_name}'
+          service_name "#{app_name}"
           action :stop
   end
 end  
